6dbc8b3a63f7ba33b51c7c6937a0d5de6f67328a
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-04-14 15:48:58 -0500

    updated ex-cred

--- js/extracredit.js
+++ js/extracredit.js
@@ -1,11 +1,30 @@
 "use strict";
 (function() {
-
+    //Simple Drills
     function returnTwo(){
         return 2;
     }
 
     console.log(returnTwo());
+
+    function sayHowdy(){
+        return "Howdy";
+    }
+
+    console.log(sayHowdy());
+
+    function returnName(yourNameHere){
+        return yourNameHere;
+    }
+
+    console.log(returnName("Luis"));
+
+    function addThree(anyNumber){
+        return anyNumber + 3;
+    }
+
+    console.log(addThree(27));
+
 
 
 }())
