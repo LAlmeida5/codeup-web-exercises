c30c79fcd117cba0310ce25f6c1a9aefe4e0f84a
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-04-22 09:46:36 -0500

    SOD

--- js/warmups.js
+++ js/warmups.js
@@ -1,3 +1,24 @@
 "use strict"
 
 console.log("Yay! I completed the warm-up!")
+
+
+
+function fizzBuzz(limit) {
+    if (limit === undefined) {          // this is David's "extra" way of doing it. leaving this out is also correct.
+        limit = 100;
+    }
+    for (let x = 1; x <= limit; x++) {
+        if (x % 15 === 0) {
+            console.log("FizzBuzz");
+        }else if (x % 5 === 0){
+            console.log("Buzz");
+        }else if (x % 3 === 0) {
+            console.log("Fizz");
+        }else {
+           console.log(x)
+        }
+    }
+}
+
+fizzBuzz();
