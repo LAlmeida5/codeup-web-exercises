5901dc456a27be91ca4d0874c6b118756a04632f
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-05-02 09:25:57 -0500

    Good Morning, completed warmup

--- js/warmups.js
+++ js/warmups.js
@@ -43,3 +43,21 @@ function alphabetize(str) {
 }
 
 console.log(alphabetize("codeup"));
+
+//warm up completion with a ternary function
+const sum=[1, 2, 3, 4, 5].reduce((partialSum, a) => partialSum + a, 0);
+console.log(sum);
+
+//or
+
+//completed with a "standard function" this was David's example.
+function sumOfElements(arr) {
+    let bucket = 0;
+    for(let el of arr) {
+        bucket += el;
+    }
+    return bucket;
+}
+
+console.log(sumOfElements([1, 2, 3, 4, 5]));
+console.log(sumOfElements([19, 27, 44, 3]));
