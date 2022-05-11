b7fac2bb9062ed91ff25b1dd3e7b9c120ab8de20
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-05-10 20:32:19 -0500

    update b/c assignment

--- js/while.js
+++ js/while.js
@@ -9,24 +9,27 @@
         }
     }
     multiplyBy2();
-
+function salesPerCustomer() {
     // This is how you get a random number between 50 and 100
     let allCones = Math.floor(Math.random() * 51) + 50;
+    let numberOfCustomers = 0;
 
     do {
+        numberOfCustomers++;
         // This expression will generate a random number between 1 and 5
         let customerOrder = Math.floor(Math.random() * 5) + 1;
         console.log("Customer orders " + customerOrder + " cone(s). We have " + allCones + " left in stock");
         if (customerOrder > allCones) {
-            console.log( "Sorry!,Your order cannot be fulfilled.");
+            console.log("Sorry!,Your order cannot be fulfilled.");
         } else {
             allCones -= customerOrder;
             console.log("Here you go! We now have " + allCones + " left in stock.");
         }
-    }while (allCones > 0);
-    console.log("We sold out of all cones! Time to go home!")
+    } while (allCones > 0);
+    console.log("Time to go! We sold out of all cones! We had " + numberOfCustomers + " customers.");
+}
 
-
+salesPerCustomer()
 
 
 })();
