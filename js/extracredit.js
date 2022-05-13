ba90a7cb4e760f322f2789defc20a237ec2b9600
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-05-13 09:01:37 -0500

    Morning update

--- js/extracredit.js
+++ js/extracredit.js
@@ -27,11 +27,67 @@
 
 // Write a function, filterNumbers() that takes in an array of mixed data types and returns an array of only the numbers type in ascencding order.
 
-    let mixedInput = ["fred", true , 5, 3];
+    const arr = ["fred", true , 5, 3];
 
-    function filterNumbers(arr) {
-        if(arr[] === )
+    const onlyNumbers = arr.filter(
+        element => typeof element === 'number'
+    );
+    console.log(onlyNumbers);
+    // Above is a solution I found online.  Looks really clean.-------
+
+    // Below is an answer with help from Dalton the Prodigy.-------
+
+    let mixedData = ['fred', true, 5, 3]
+    function filterNumbers(param) {
+        if (typeof param !== 'object')
+             return;
+        let bucket = [];
+        for (const el of param) {
+            if (typeof el === 'number') bucket.push(el);
+        }
+        return bucket;
     }
+
+    console.log(filterNumbers(mixedData));
+
+    let dogStats = [
+        {
+            name: "Chompers",
+            breed: "Pug",
+            age: 7
+        },
+        {
+            name: "Freddy",
+            breed: "Lab",
+            age: 4
+        },
+        {
+            name: "Mr. Pig",
+            breed: "Mastif",
+            age: 10
+        }
+    ];
+
+    function getOlder(arr) {
+        for(let i = 0; i < arr.length; i++) {  // arr.length is used to iterate through the array objects.
+            arr[i].age += 1;        // arr[i] is used to access the array objects // .age accesses the "age property" += 1 added one to eat age property within the object.
+        }
+        return arr;                     // returns the changes in the array.
+    }
+
+    console.log(getOlder(dogStats));        //calls the function and passes the array.
+
+// or this way...
+
+    //     function getOlder(arr){         // Basically this for loop is iterating through each object within the array.
+    //     for(let obj of arr){                // obj is an element, you can name this anything you want.  Typically use single of plural i.e. (let bread of breads)
+    //         obj.age += 1;
+    //     }
+    //     return arr;
+    // }
+    //
+    // console.log(getOlder(dogStats));
+
 
 }())
 
