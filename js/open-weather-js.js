43451393c4931ff17bd3c3ab8ab18994224888d5
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-06-01 16:39:31 -0500

    cards completed

--- js/open-weather-js.js
+++ js/open-weather-js.js
@@ -1,9 +1,19 @@
-const URL = "https:api.openweathermap.org/data/2.5/onecall";
+"use strict";
+
+mapboxgl.accessToken = MAPBOX_API_KEY;
+const MAP = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [-98.4946, 29.4252], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
+
+const URL = "http://api.openweathermap.org/data/2.5/onecall";
 
 $.get(URL, {
     APPID: OPEN_WEATHER_KEY,
-    lat: 40.7485452,
-    lon: -73.9879522,
+    lat: 29.4252,
+    lon: -98.4946,
     units: "imperial"
 }).done(function(results) {console.log(results)})
 
@@ -18,8 +28,28 @@ geocode("San Antonio, US", MAPBOX_API_KEY).then(function(results) {
         units: "imperial"
     }).done(function(results) {
         console.log(results)
-        let x = 1;
+        let x = 0;
         let y = 4;
+        results.daily.forEach(function(card) {
+            if(x <= y) {
+                //first const is to accepts the date format from weatherAPI "property" multiplies by 1000 and converts it to current new date.
+                const date = new Date(card.dt * 1000);
+                //this const takes the date variable and attaches data from weather API "property" to a new format array.
+                const [month, day, year] = [ date.getDay(), date.getMonth(), date.getFullYear()];
+                let imgSrc = 'http://openweathermap.org/img/w/[icon].png' + card.weather[0].icon + '.png';
+                let cards = '<div class="card col-2 m-auto" >' +
+                    '<div class="card-header">' + [month, day, year] + imgSrc + '</div>' +
+                    '<ul class="list-group list-group-flush">' +
+                    '<li class="list-group-item">' + card.weather[0].description + '</li>' +
+                    '<li class="list-group-item">' + 'Humidity:' + card.humidity + '</li>' +
+                    '<li class="list-group-item">' + 'Wind Speed' + card.wind_speed.toFixed() + 'Mph' + '</li>' +
+                    '</ul>' +
+                    '</div>';
+                //Takes completed card and puts it into my div with id weatherCards. This will take place for each iteration.
+                $('#weatherCards').append(cards);
+                x++;
+            }
+        })
 
     })
 });
