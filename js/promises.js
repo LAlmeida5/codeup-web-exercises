6fb5b8c74ac02ad674f107ac95f392fa8265015c
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-06-06 11:21:57 -0500

    completed promise exercise

--- js/promises.js
+++ js/promises.js
@@ -1,3 +1,14 @@
 "use strict";
 
-fetch(url, {headers: {'Authorization': 'PROMISES_GH_KEY'}})
+let userInput = prompt("Enter your username")
+const url = `https://api.github.com/users/${userInput}/events/public`
+fetch(url, {headers: {Authorization: `token ${PROMISES_GH_KEY}`}})
+    .then((resp) => resp.json())
+    .then((data) => {
+        console.log(`${data[0].created_at}`)
+        let userInfo = `Username: ${data[0].actor.login} Last Commit: ${data[0].created_at.substring(0,10)}`;
+        let comMsg= `Commit Message: ${data[0].payload.commits[0].message}`;
+        console.log(userInfo);
+        document.getElementById('first').append(userInfo);
+        document.getElementById('second').append(comMsg);
+})
