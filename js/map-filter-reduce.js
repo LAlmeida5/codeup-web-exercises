e3954df6fed9d32583a41a1f81d9a0e66ec6e788
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-06-14 15:54:14 -0500

    EOD

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,58 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+//#2
+const resLang = users.filter(user => user.languages.length >= 3);
+console.log(resLang);
+
+//longer form than arrow function, still shorter than doing it traditionally.
+// const resLang = users.filter(function (user){
+//     return user.languages.length >= 3;
+// })
+
+//This is so i can visualize what .filter is doing above!
+// let resLangBucket = [];
+// for (let i = 0; i < users.length; i++) {
+//     if(users[i].languages.length >= 3) resLangBucket.push(users[i])
+// }
+
+//#3
+const getEmail = users.map(user => user.email);
+console.log(getEmail);
+
+//#4
