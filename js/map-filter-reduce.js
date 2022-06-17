f6056107c98e38591ad06c54a9c141882c5a283d
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-06-16 22:15:02 -0500

    Update/completed MapFilterReduce exercises

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -45,7 +45,7 @@ console.log(resLang);
 //     return user.languages.length >= 3;
 // })
 
-//This is so i can visualize what .filter is doing above!
+//This is so I can visualize what .filter is doing above!
 // let resLangBucket = [];
 // for (let i = 0; i < users.length; i++) {
 //     if(users[i].languages.length >= 3) resLangBucket.push(users[i])
@@ -64,14 +64,56 @@ console.log(yrsExp);
 
 //#5
 
-const longEmail = (previous, user) => {
-    if (previous.email.length > user.email.length) {
-        return previous;
-    }else {
+const longEmail = users.reduce((total, user) => {
+    let data = user.email.length - total;
+    if (data === user.email.length) {
         return user;
+    } else {
+        return user.email;
     }
-}
+}, users[0]);
 
-const longestEmail = users.reduce(longEmail, {email:[]});
+console.log(longEmail);
 
-console.log(longestEmail);
+//#6
+
+const instructorIntros = users.reduce((string, user, index) => {
+    if (index === users.length - 1) {
+        return `${string}and ${user.name}.`;
+    } else {
+        return `${string}${user.name}, `;
+    }
+}, "Here are your cohort instructors: ");
+
+console.log(instructorIntros);
+
+
+// ***Bonus: copied from walk-through for reference purposes only.***
+// David's code
+
+const uniqueLangs = users.reduce((langs, user) => {
+    for (let lang of user.languages) {
+        if (!langs.includes(lang)) { // langs.includes(lang) === false
+            langs.push(lang);
+        }
+    }
+    return langs;
+}, []);
+
+console.log(uniqueLangs);
+
+const davidsCoolWayToGetUniqueLanguages = Array.from(new Set(users.reduce((langs, user) => {
+            for (let lang of user.languages) {
+                langs.push(lang);
+            }
+            return langs;
+        }, [])
+    )
+);
+
+console.log(davidsCoolWayToGetUniqueLanguages);
+
+
+
+
+
